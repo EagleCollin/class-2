bf0054343c4861cfa0142edbf1c89884b8620222
EagleCollin <scoutcollin97@gmail.com>
2022-02-22 13:04:16 -0600

    notes for part 2

---
+++ app.js
@@ -0,0 +1,15 @@
+const foods = [
+    {
+        name:pork,
+        price: 5
+    },
+
+]
+
+//loop over the array
+for(let food of foods){
+    console.log(food.name);
+}
+//creat a list item (<li></li>)
+//insert food name into list item element
+//appent list item to <ul></ul>
---
+++ class-2-exercise1.js
@@ -0,0 +1,12 @@
+const string = "this is a string"
+const number = 5
+const boolean = false
+const user = null
+const dog = undefined
+const object = {
+    computer: "mine",
+    mouse: "logitech",
+    keyboard: "corsair"
+}
+const array = [Jerry, Jenny, Jackie]
+const cat
